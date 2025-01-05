09558e826738bca751d778c64a5d9e5cc25fbaea
pedro <96991500+px-bin@users.noreply.github.com>
2025-01-05 00:30:58 -0300

    crdroid bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The aospOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_d2x.mk
+    $(LOCAL_DIR)/lineage_d2x.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_d2x-eng \
-    aosp_d2x-user \
-    aosp_d2x-userdebug
+    lineage_d2x-eng \
+    lineage_d2x-user \
+    lineage_d2x-userdebug
