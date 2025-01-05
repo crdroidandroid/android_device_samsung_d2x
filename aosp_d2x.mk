eb88876ea44134a2db8b444e06428f935ed7bea6
pedro <96991500+px-bin@users.noreply.github.com>
2025-01-05 00:28:57 -0300

    crdroid bringup

--- aosp_d2x.mk
+++ aosp_d2x.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The aospOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -37,25 +37,22 @@ TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
 
 ## Inherit some common aosp stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Project-Elixir
+# crDroid stuff
 IS_PHONE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BUILD_APERTURE_CAMERA := true
-FORCE_LAWNCHAIR := true
-ELIXIR_BUILD_TYPE := OFFICIAL
-BUILD_USERNAME := FrEeRuNnEr4EvEr
-BUILD_HOSTNAME := Elixir
 TARGET_INCLUDE_STOCK_ACORE := false
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_HAS_UDFPS :=  true
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := aosp_d2x
+PRODUCT_NAME := lineage_d2x
 PRODUCT_DEVICE := d2x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N976B
